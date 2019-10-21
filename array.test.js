f861f9d36464632054b06ab8df0d7b2d5761c6b5
Bogdan <bybogdan@mail.ru>
2019-10-21 17:01:41 +0300

    third commit

--- array.test.js
+++ array.test.js
@@ -1,123 +1,229 @@
-describe('Array', () => {
-  it('Should find the position of the first occurrence', () => {
-    const arr1 = [1, 5, 8, 3, 2];
-    expect(/* Find 5 position*/).toBe(1);
-    expect(/* Find 3 position */).toBe(/* ??? */);
-
-    // TODO: Write additional its
+ describe('Array', () => {
+    it('Should find the position of the first occurrence', () => {
+      const arr1 = [1, 5, 8, 3, 2];
+      expect(arr1.indexOf(5)).toBe(1);
+      expect(arr1.indexOf(3)).toBe(3);
+      expect(arr1.indexOf(9)).toBe(-1);
+      expect(arr1.indexOf(2)).toBe(4);
+    });
+  
+    it('Should return the specified array twice', () => {
+      let arr1 = [1, 2, 3];
+      let arr2 = [0];
+      expect([...arr1, ...arr1]).toStrictEqual([1, 2, 3, 1, 2, 3]);
+      expect([...arr2, ...arr2]).toStrictEqual([0, 0]);
+    });
+  
+    it('Convert the number array to  the array of string values', () => {
+      function convertToStringArr(arr) {
+        return arr.map(i => typeof(i) == 'object' ? JSON.stringify(i) : i += '');
+      }
+  
+      expect(convertToStringArr([1, 2, 3])).toStrictEqual(['1', '2', '3']);
+      expect(convertToStringArr(['a', 'b', 'c'])).toStrictEqual(['a', 'b', 'c']);
+      expect(convertToStringArr([{a:1}, {b:2}, {c:3}])).toStrictEqual(['{"a":1}', '{"b":2}', '{"c":3}']);
+    });
+  
+    it('Should return the number of all occurrences of specified item in an array', () => {
+      function calculateOccurences(arr, value) {
+        return arr.filter(i => i === value).length;
+      }
+      
+      expect(calculateOccurences([1, 2, 1, 4, 1], 1)).toBe(3);
+      expect(calculateOccurences([1, 1, 1, 1, 1], 1)).toBe(5);
+      expect(calculateOccurences([], 1)).toBe(0);
+    });
+  
+    it('Should convert strings from specified array to uppercase', () => {
+      function toUppercase(arr) {
+        return arr.map(i => i.toUpperCase());
+      }
+      expect(toUppercase(["aaaa", "abc"])).toStrictEqual(['AAAA', 'ABC']);
+    });
+  
+    it('Insert an item at specified position', () => {
+      function insert(arr, value, pos) {
+        arr.splice(pos, 0, value);
+        return arr;
+      }
+      expect(insert([1, 2, 4], 3, 2)).toStrictEqual([1, 2, 3, 4]);
+    });
+  
+    it('Should return the n last items from the specified array', () => {
+      function last(arr, pos) {
+        return arr.splice(arr.length - pos, pos);
+      }
+      expect(last([1, 2, 3, 4, 5, 6, 7], 3)).toStrictEqual([5, 6, 7]);
+    });
+  
+    it('Return the 3 largest items from integer array', () => {
+      function find3Largest(arr) {
+        let res = [];
+        let sortArr = (arr.slice()).sort((a, b) => a > b).splice(arr.length - 3);
+        arr.forEach(i => sortArr.includes(i) && res.push(i));
+        return res;
+      }
+      expect(find3Largest([1, 3, 8, 3, 29, 11, 2, 17, 9, 1])).toStrictEqual(
+        [29, 11, 17]
+      );
+    });
+  
+    it('Sort numbers array by using array method', () => {
+      function sort1(arr) {
+        return arr.sort((a, b) => a < b);
+      }
+      function sort2(arr) {
+        let flag = false;
+        while (!flag) {
+          flag = true;
+          for (let j = 1; j < arr.length; j++) {
+            if (arr[j - 1] < arr[j]) {
+              flag = false;
+              [arr[j - 1], arr[j]] = [arr[j], arr[j - 1]];
+            }
+          }
+        }
+        return arr;
+      }
+      expect(sort1([2, 3, 1, 8, 4, 5])).toStrictEqual([8, 5, 4, 3, 2, 1]);
+      expect(sort2([2, 3, 1, 8, 4, 5])).toStrictEqual([8, 5, 4, 3, 2, 1]);
+    });
+  
+    it('Should summarize of all items of numbers array', () => {
+      function sum(arr) {
+        return arr.reduce((acc, curr) => acc + curr);
+      }
+      expect(sum([1, 5, 7, 9, 3])).toBe(25);
+    });
+  
+    it('Should return the numbers of falsy value in the specified array', () => {
+      function getNumberOfFalsy(arr) {
+        return arr.map(i => !i).filter(j => j).length;
+      }
+      expect(getNumberOfFalsy([1, 0, "", null, "hello", "0"])).toBe(3);
+    });
+  
+    it('Should return an array of unique items from the specified array', () => {
+      function unique1(arr) {
+        return arr.filter((v, i, a) => a.indexOf(v) === i);
+      }
+      function unique2(arr) {
+        return [...new Set(arr)];
+      }
+      expect(unique1(["a", "b", "a", "c", "e", "b", "o"])).toStrictEqual([
+        'a',
+        'b',
+        'c',
+        'e',
+        'o'
+      ]);
+      expect(unique2(["a", "b", "a", "c", "e", "b", "o"])).toStrictEqual([
+        'a',
+        'b',
+        'c',
+        'e',
+        'o'
+      ]);
+    });
+  
+    it('Should return a map of grouped data by key and value selector', function() {
+      function group(arr, key) {
+        let res = [];
+        let ulist = new Set();
+        let other = Object.keys(arr[0]).filter(i => i != key);
+        arr.forEach(o => ulist.add(o[key]));
+        res = [...ulist].map(i => [i]);
+        ulist.forEach(i => {
+          let list = [];
+          arr.forEach(o => o[key] === i && list.push(o[other]));
+          res[[...ulist].indexOf(i)].push(list);
+        });
+        return res;
+      }
+  
+      let arr = [
+        { country: 'Belarus', city: 'Brest' },
+        { country: 'Russia', city: 'Omsk' },
+        { country: 'Russia', city: 'Samara' },
+        { country: 'Belarus', city: 'Grodno' },
+        { country: 'Belarus', city: 'Minsk' },
+        { country: 'Poland', city: 'Lodz' }
+      ];
+  
+      expect(group(arr, 'country')).toStrictEqual([
+        ['Belarus', ['Brest', 'Grodno', 'Minsk']],
+        ['Russia', ['Omsk', 'Samara']],
+        ['Poland', ['Lodz']]
+      ]);
+    });
+  
+    it('Should creates an array with all falsy values removed.', () => {
+      function compact(arr) {
+        return arr.filter(i => !!i);
+      }
+      expect(compact([1, 0, null, "a"])).toStrictEqual([1, 'a']);
+    });
+  
+    it('Should flattens array a single level deep', () => {
+      let res = [];
+      function flatten(arr) {
+        if (typeof(arr[0]) != 'object' && typeof(arr[arr.length - 1]) != 'object') {
+          arr.forEach(i => res.push(i));
+        } else {
+          arr.forEach(i => (typeof(i) === 'object') ? flatten(i) : res.push(i));
+        }
+        return res;
+      }
+      expect(flatten([1, [2, [3, [4]], 5], 7])).toStrictEqual([
+        1,
+        2,
+        [3, [4]],
+        5
+      ]);
+    });
+  
+    it('Should recursively flattens array.', () => {
+      let res = [];
+      function flattenDeep(arr) {
+        arr.forEach(i => (typeof(i) === 'object') ? flattenDeep(i) : res.push(i));
+        return res;
+      }
+      expect(flattenDeep([1, [2, [3, [4]], 5]])).toStrictEqual([
+        1,
+        2,
+        3,
+        4,
+        5
+      ]);
+    });
+  
+    it('Should creates an array of unique values that are included in all given', () => {
+      function intersection(arr1, arr2) {
+        return arr1.filter(value => arr2.includes(value));
+      }
+      expect(intersection([1, 2, 3, 4], [8, 3, 1, 0, 9])).toStrictEqual([
+        1,
+        3
+      ]);
+    });
+  
+    it('Should remove all elements from array that predicate returns truthy for and returns an array of the removed elements. The predicate is invoked with two arguments: (value, index).', () => {
+      const arr = [1, 7, 5, 2, 8];
+      const gt5 = v => v > 5;
+  
+      function remove(arr, cond) {
+        let deleted = [];
+        arr.map((v, i) => {
+          if (cond(v)) {
+            deleted.push(v);
+            arr.splice(i, 1);
+          }
+        })
+        return deleted;
+      }
+  
+      let removed = remove(arr, gt5);
+      expect(arr).toStrictEqual([1, 5, 2]);
+      expect(removed).toStrictEqual([7, 8]);
+    });
   });
-
-  it('Should return the specified array twice', () => {
-    expect(/* double([1, 2, 3])*/).toStrictEqual([1, 2, 3, 1, 2, 3]);
-
-    // TODO: Write additional its
-  });
-
-  it('Convert the number array to  the array of string values', () => {
-    expect(/* convertToStringArr([1, 2, 3]) */).toStrictEqual(['1', '2', '3']);
-
-    // TODO: Write additional its
-  });
-
-  it('Should return the number of all occurrences of specified item in an array', () => {
-    expect(/* calculateOccurences([1, 2, 1, 4, 1], 1)*/).toBe(3);
-
-    // TODO: Write additional its
-  });
-
-  it('Should convert strings from specified array to uppercase', () => {
-    expect(/* toUppercase(["aaaa", "abc"])*/).toStrictEqual(['AAAA', 'ABC']);
-  });
-
-  it('Insert an item at specified position', () => {
-    expect(/* insert([1, 2, 4], 3, 2)*/).toStrictEqual([1, 2, 3, 4]);
-  });
-
-  it('Should return the n last items from the specified array', () => {
-    expect(/* last([1, 2, 3, 4, 5, 6, 7], 3) */).toStrictEqual([5, 6, 7]);
-  });
-
-  it('Return the 3 largest items from integer array', () => {
-    expect(/* find3Largest([1, 3, 8, 3, 29, 11, 2, 17, 9, 1]) */).toStrictEqual(
-      [29, 11, 17]
-    );
-  });
-
-  it('Sort numbers array by using array method', () => {
-    expect(/* sort([2, 3, 1, 8, 4, 5] ) */).toStrictEqual([8, 5, 4, 3, 2, 1]);
-  });
-
-  it('Should summarize of all items of numbers array', () => {
-    expect(/* sum([1, 5, 7, 9, 3]) */).toBe(25);
-  });
-
-  it('Should return the numbers of falsy value in the specified array', () => {
-    expect(/* getNumberOfFalsy([1, 0, "", null, "hello", "0"]) */).toBe(3);
-  });
-
-  it('Should return an array of unique items from the specified array', () => {
-    expect(/* unique(["a", "b", "a", "c", "e", "b", "o"]) */).toStrictEqual([
-      'a',
-      'b',
-      'c',
-      'e',
-      'o'
-    ]);
-  });
-
-  it('Should return a map of grouped data by key and value selector', function() {
-    let arr = [
-      { country: 'Belarus', city: 'Brest' },
-      { country: 'Russia', city: 'Omsk' },
-      { country: 'Russia', city: 'Samara' },
-      { country: 'Belarus', city: 'Grodno' },
-      { country: 'Belarus', city: 'Minsk' },
-      { country: 'Poland', city: 'Lodz' }
-    ];
-
-    expect(/* group(arr, 'country') */).toStrictEqual([
-      ['Belarus', ['Brest', 'Grodno', 'Minsk']],
-      ['Russia', ['Omsk', 'Samara']],
-      ['Poland', ['Lodz']]
-    ]);
-  });
-
-  it('Should creates an array with all falsy values removed.', () => {
-    expect(/* compact([1, 0, null, "a"]) */).toStrictEqual([1, 'a']);
-  });
-
-  it('Should flattens array a single level deep', () => {
-    expect(/* flatten([1, [2, [3, [4]], 5]]) */).toStrictEqual([
-      1,
-      2,
-      [3, [4]],
-      5
-    ]);
-  });
-
-  it('Should recursively flattens array.', () => {
-    expect(/* flattenDeep([1, [2, [3, [4]], 5]]) */).toStrictEqual([
-      1,
-      2,
-      3,
-      4,
-      5
-    ]);
-  });
-
-  it('Should creates an array of unique values that are included in all given', () => {
-    expect(/* intersection([1, 2, 3, 4], [8, 3, 1, 0, 9]) */).toStrictEqual([
-      1,
-      3
-    ]);
-  });
-
-  it('Should remove all elements from array that predicate returns truthy for and returns an array of the removed elements. The predicate is invoked with two arguments: (value, index).', () => {
-    const arr = [1, 7, 5, 2, 8];
-    const gt5 = v => v > 5;
-
-    let removed; /* remove(arr, gt5); */
-    expect(arr).toStrictEqual([1, 5, 2]);
-    expect(removed).toStrictEqual([7, 8]);
-  });
-});
--- object.test.js
+++ object.test.js
@@ -2,7 +2,11 @@ const PUT_ANSWER_HERE = Symbol();
 
 describe('Objects', () => {
   it('Should get the value at path of object. If the resolved value is undefined, the defaultValue is returned in its place.', () => {
-    function get(obj, path) {}
+    function get(obj, path) {
+      let res = obj;
+      path.split('.').forEach(i => { if (obj[i]) res = obj = obj[i] });
+      return res;
+    }
 
     expect(get({ a: { b: { c: 3 } } }, 'a')).toStrictEqual({ b: { c: 3 } });
     expect(get({ a: { b: { c: 3 } } }, 'a.b.c')).toBe(3);
@@ -13,7 +17,13 @@ describe('Objects', () => {
   });
 
   it('Creates an object composed of the picked object properties.', () => {
-    function pick(obj, props) {}
+    function pick(obj, props) {
+      let newObj = {};
+      props.forEach(i => {
+        newObj[i] = obj[i];
+      })
+      return newObj;
+    }
     const object = { a: 1, b: '2', c: 3 };
 
     expect(pick(object, ['a', 'c'])).toStrictEqual({ a: 1, c: 3 });
@@ -21,17 +31,29 @@ describe('Objects', () => {
   });
 
   it('Should clone object', () => {
+    function clone1(obj) {
+      return Object.assign({}, obj);
+    }
+    function clone2(obj) {
+      return JSON.parse(JSON.stringify(obj));
+    }
+
     const person1 = {
       firstName: 'Ivan',
       secondName: 'Ivanov'
     };
-
-    const person2 = person1;
+    let person2 = {};
+    let person3 = {};
+    person2 = clone1(person1);
     person2.firstName += ' Jr.';
+    person3 = clone2(person1);
+    person3.firstName += ' Jr.';
 
     expect(person1.firstName).toBe('Ivan');
     expect(person2.firstName).toBe('Ivan Jr.');
     expect(person2.secondName).toBe('Ivanov');
+    expect(person3.firstName).toBe('Ivan Jr.');
+    expect(person3.secondName).toBe('Ivanov');
   });
 
   it('Performs a shallow comparison between two values to determine if they are equivalent.', () => {
@@ -39,20 +61,28 @@ describe('Objects', () => {
     const obj2 = { a: 1, b: 2 };
     const obj3 = { a: 1, b: 4 };
 
-    expect(/* compare(obj1, obj2) */).toBe(true);
-    expect(/* compare(obj1, obj3) */).toBe(false);
+    function compare(o1, o2) {
+      return JSON.stringify(o1) === JSON.stringify(o2);
+    }
+
+    expect(compare(obj1, obj2)).toBe(true);
+    expect(compare(obj1, obj3)).toBe(false);
   });
 
   it('Performs a deep comparison between two values to determine if they are equivalent.', () => {
     const obj1 = { a: 1, b: { a: 2 } };
     const obj2 = { a: 1, b: { a: 2 } };
 
-    expect('/* compare(obj1, obj2) */').toBe(true);
+    function compare(o1, o2) {
+      return JSON.stringify(o1) === JSON.stringify(o2);
+    }
+
+    expect(compare(obj1, obj2)).toBe(true);
   });
 
   it('Fix me', () => {
     function hasAccess(role) {
-      if (role == { type: 'admin' }) {
+      if (role.type === 'admin') {
         return true;
       } else {
         return false;
